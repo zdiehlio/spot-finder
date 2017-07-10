35bbfd366666674ec51b978f50911a963366c5fd
Zachary Diehl <zachary.diehl@gmail.com>
2017-07-10 16:31:34 -0700

    created user and functionality for token and password hash, working on testing

--- test/user-test.js
+++ test/user-test.js
@@ -2,19 +2,21 @@ const dotenv = require('dotenv')
 dotenv.config({ path: `${__dirname}/../.test.env`})
 const expect = require('expect')
 const superagent = require('superagent')
+const User = require('../model/user.js')
 
 const server = require('../lib/server.js')
 
 const ROOT_URL = `http://localhost:${process.env.PORT}`
 
-describe('Testing user routes', () => {
+describe('Testing User', () => {
+  console.log('Testing')
   before(server.start)
   after(server.stop)
 
-  describe('Testing Post Controller', () => {
-    it('Should create user Auth and return 200 status', () => {
+  describe('Testing user constructor', () => {
+    it.only('Should create user Auth and return 200 status', () => {
       return superagent.post(`${ROOT_URL}/api/users`)
-        .send({})
+        .send(new User())
         .then(res => {
           expect(res.body.tokenSeed).toExist()
           expect(res.body.passwordHash).toExist()
