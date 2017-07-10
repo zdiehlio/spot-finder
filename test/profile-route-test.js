3e762693188dcc6ee7c271b44c386faa9f7dba34
adan.rabadan <evilb4k4@gmail.com>
2017-07-10 14:51:14 -0700

    got the test working

--- test/profile-route-test.js
+++ test/profile-route-test.js
@@ -1,22 +1,32 @@
-require('dotenv').config({ path: `${__dirname}/../.test.env` })
+const dotenv = require('dotenv')
+dotenv.config({ path: `${__dirname}/../.test.env`})
 const expect = require('expect')
 const superagent = require('superagent')
-const API_URL = process.env.API_URL
-describe('testing POST /api/profile', () => {
 
-  it('should respond with a profile', () => {
-    return superagent.post(`${API_URL}/api/profile`)
-      .send()
-      .then(res => {
-        expect(res.status).toEqual(200)
-        expect(res.body.username).toEqual(' ')
-        expect(res.body._id).toExist()
+
+const ROOT_URL = `http://localhost:${process.env.PORT}`
+const server = require('../lib/server.js')
+
+describe('start server', () => {
+  before(server.start)
+  after(server.stop)
+
+  describe('testing POST /api/profile', () => {
+    it('should respond with a profile', () => {
+      return superagent.post(`${ROOT_URL}/api/profile`)
+        .send()
+        .end(res => {
+          expect(res.status).toEqual(200)
+          expect(res.body.username).toEqual(' ')
+          expect(res.body._id).toExist()
+        })
+    })
+
+    it('should respond with 400 invalid request body', () => {
+      return superagent.post(`${ROOT_URL}/api/profile`).send().catch(err => {
+        expect(err.status).toEqual(404)
       })
-  })
-
-  it('should respond with 400 invalid request body', () => {
-    return superagent.post(`${API_URL}/api/profile`).send().catch(err => {
-      expect(err.status).toEqual(400)
     })
   })
+
 })
