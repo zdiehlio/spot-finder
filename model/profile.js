f8d5cde00e00d40ed31333bd9a461b20ca5f34d7
adan.rabadan <evilb4k4@gmail.com>
2017-07-10 12:15:24 -0700

    fixed lint for profile

--- model/profile.js
+++ model/profile.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const profileSchema = mongoose.Schema({
   username: {type: String, required: true, minlength: 1},
   userID: {type: mongoose.Schema.Types.ObjectId, required: true},
   ownedVenues: [{type: mongoose.Schema.Types.ObjectId}],
   ownedEvents: [{type: mongoose.Schema.Types.ObjectId}],
-});
+})
 
-module.exports = mongoose.model('profile', profileSchema);
+module.exports = mongoose.model('profile', profileSchema)
--- test/profile-route-test.js
+++ test/profile-route-test.js
@@ -1,24 +1,22 @@
-require('dotenv').config({ path: `${__dirname}/../.test.env` });
-const expect = require('expect');
-const superagent = require('superagent');
-
+require('dotenv').config({ path: `${__dirname}/../.test.env` })
+const expect = require('expect')
+const superagent = require('superagent')
+const API_URL = process.env.API_URL
 describe('testing POST /api/profile', () => {
-  console.log('data', data);
 
   it('should respond with a profile', () => {
     return superagent.post(`${API_URL}/api/profile`)
-    .send(data)
-    .then(res => {
-      console.log('data', data);
-      expect(res.status).toEqual(200);
-      expect(res.body.username).toEqual(data.username);
-      expect(res.body._id).toExist();
-    });
-  });
+      .send()
+      .then(res => {
+        expect(res.status).toEqual(200)
+        expect(res.body.username).toEqual(' ')
+        expect(res.body._id).toExist()
+      })
+  })
 
   it('should respond with 400 invalid request body', () => {
     return superagent.post(`${API_URL}/api/profile`).send().catch(err => {
-      expect(err.status).toEqual(400);
-    });
-  });
-});
+      expect(err.status).toEqual(400)
+    })
+  })
+})
