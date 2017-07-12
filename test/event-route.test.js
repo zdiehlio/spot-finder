a91f71699173c54b2291b18e10f58d1ff5d4187e
Ryan Felix <therjfelix@gmail.com>
2017-07-12 12:00:57 -0700

    add further event route tests

--- test/event-route.test.js
+++ test/event-route.test.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 dotenv.config({ path: `${__dirname}/../.test.env`})
 const superagent = require('superagent')
 const expect = require('expect')
+const moment = require('moment')
 
 const server = require('../lib/server.js')
 const mockEvent = require('./lib/mock-event.js')
@@ -26,6 +27,7 @@ describe('event routes', () => {
   after(() => server.stop())
 
   let testEvent
+  let testEventId
   it('should create a event', () => {
     return Promise.resolve(mockEvent.createOneTestCase())
       .then(event => testEvent = event)
@@ -35,9 +37,67 @@ describe('event routes', () => {
           .then(res => {
             expect(res.status).toEqual(201)
             expect(res.body.name).toEqual(testEvent.name)
-            console.log(res.body)
+            expect(res.body._id).toExist()
+            testEventId = res.body._id
           })
       })
+  })
+
+  it('should reject and respond 400 to invalid event', () => {
+    return superagent.post(ENDPOINT)
+      .send({
+        fun: 'yes',
+        freeBeer: 'no',
+      })
+      .catch(err => expect(err.status).toEqual(400))
+  })
+
+  it('should reject and respond 400 to an event with nonsense times', () => {
+    return superagent.post(ENDPOINT)
+      .send({
+        name: 'bad times',
+        numberOfPeople: 2,
+        start: moment().add(5, 'hours'),
+        end: moment().add(3, 'hours'),
+      })
+      .catch(err => expect(err.status).toEqual(400))
+  })
+
+  it('should reject and respond 400 to an event assigned to a nonexistent venue', () => {
+    return superagent.post(ENDPOINT)
+      .send({
+        name: 'party',
+        numberOfPeople: 1,
+        start: moment().add(1, 'hour'),
+        end: moment().add(2, 'hours'),
+        venue: 12345,
+      })
+      .catch(err => expect(err.status).toEqual(400))
+  })
+
+  it('should read an event', () => {
+    return superagent.get(`${ENDPOINT}/${testEventId}`)
+      .then(res => {
+        expect(res.status).toEqual(200)
+        expect(res.body.name).toEqual(testEvent.name)
+      })
+  })
+
+  const updatedEvent = {
+    name: 'big shindig',
+  }
+  it('should update an event', () => {
+    return superagent.put(`${ENDPOINT}/${testEventId}`)
+      .send(updatedEvent)
+      .then(res => {
+        expect(res.status).toEqual(200)
+        expect(res.body.name).toEqual(updatedEvent.name)
+      })
+  })
+
+  it('should delete an event', () => {
+    return superagent.delete(`${ENDPOINT}/${testEventId}`)
+      .then(res => expect(res.status).toEqual(204))
   })
 
 })
